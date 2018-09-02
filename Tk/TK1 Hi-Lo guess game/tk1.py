70848cbbda4fe8e30d4b2a98c54fd86c4068e2aa
French Bear <FrenchBear38@outlook.com>
2018-09-02 12:50:18 +0200

    tk progress

--- Tk/TK1 Hi-Lo guess game/tk1.py
+++ Tk/TK1 Hi-Lo guess game/tk1.py
@@ -5,7 +5,8 @@
 # 2018-08-31    PV
 
 import random
-from tkinter import Tk, Label, Button, Entry, StringVar, DISABLED, NORMAL, END, W, E
+import tkinter as tk
+
 
 class GuessingGame:
     def __init__(self, master):
@@ -17,18 +18,18 @@ class GuessingGame:
         self.num_guesses = 0
 
         self.message = "Guess a number from 1 to 100"
-        self.label_text = StringVar()
+        self.label_text = tk.StringVar()
         self.label_text.set(self.message)
-        self.label = Label(master, textvariable=self.label_text)
+        self.label = tk.Label(master, textvariable=self.label_text)
 
         vcmd = master.register(self.validate) # we have to wrap the command
-        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+        self.entry = tk.Entry(master, validate="key", validatecommand=(vcmd, '%P'))
 
-        self.guess_button = Button(master, text="Guess", command=self.guess_number)
-        self.reset_button = Button(master, text="Play again", command=self.reset, state=DISABLED)
+        self.guess_button = tk.Button(master, text="Guess", command=self.guess_number)
+        self.reset_button = tk.Button(master, text="Play again", command=self.reset, state=tk.DISABLED)
 
-        self.label.grid(row=0, column=0, columnspan=2, sticky=W+E)
-        self.entry.grid(row=1, column=0, columnspan=2, sticky=W+E)
+        self.label.grid(row=0, column=0, columnspan=2, sticky=tk.W+tk.E)
+        self.entry.grid(row=1, column=0, columnspan=2, sticky=tk.W+tk.E)
         self.guess_button.grid(row=2, column=0)
         self.reset_button.grid(row=2, column=1)
 
@@ -56,8 +57,8 @@ class GuessingGame:
         elif self.guess == self.secret_number:
             suffix = '' if self.num_guesses == 1 else 'es'
             self.message = "Congratulations! You guessed the number after %d guess%s." % (self.num_guesses, suffix)
-            self.guess_button.configure(state=DISABLED)
-            self.reset_button.configure(state=NORMAL)
+            self.guess_button.configure(state=tk.DISABLED)
+            self.reset_button.configure(state=tk.NORMAL)
 
         elif self.guess < self.secret_number:
             self.message = "Too low! Guess again!"
@@ -67,7 +68,7 @@ class GuessingGame:
         self.label_text.set(self.message)
 
     def reset(self):
-        self.entry.delete(0, END)
+        self.entry.delete(0, tk.END)
         self.secret_number = random.randint(1, 100)
         self.guess = 0
         self.num_guesses = 0
@@ -75,9 +76,9 @@ class GuessingGame:
         self.message = "Guess a number from 1 to 100"
         self.label_text.set(self.message)
 
-        self.guess_button.configure(state=NORMAL)
-        self.reset_button.configure(state=DISABLED)
+        self.guess_button.configure(state=tk.NORMAL)
+        self.reset_button.configure(state=tk.DISABLED)
 
-root = Tk()
+root = tk.Tk()
 my_gui = GuessingGame(root)
 root.mainloop()
--- Tk/TK2 Inch to cm converter/tk2.py
+++ Tk/TK2 Inch to cm converter/tk2.py
@@ -4,24 +4,26 @@
 #
 # 2018-08-31    PV
 
-from tkinter import Frame, Label, Entry, StringVar, Button, messagebox, Menu, SOLID
+import tkinter as tk
+import tkinter.messagebox as tkmb
 import re
 
 NUMBER_RE = re.compile(r"(\+|-)?((\d+)|(\d+\.\d*)|(\d*\.\d+))")
 
 
 # Extends the Frame class.
-class MyQuitter(Frame):
-    def __init__ (self, master = None):
-        Frame.__init__(self, master)
-        Button(self, text = "Quit",  command = self.myquit, borderwidth=1, relief=SOLID).grid()
+class MyQuitter(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        tk.Button(self, text="Quit",  command=self.myquit,
+                  borderwidth=1, relief=tk.SOLID).grid()
 
     def myquit(self):
-        if messagebox.askokcancel("Quit", "Do you really wish to quit?"):
-            Frame.quit(self)
+        if tkmb.askokcancel("Quit", "Do you really wish to quit?"):
+            tk.Frame.quit(self)
 
 
-class MyApp(Frame):
+class MyApp(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
@@ -29,33 +31,39 @@ class MyApp(Frame):
         self.master.minsize(300, 200)
         self.master.title("Convert in -> cm")
 
-
+        # Menu
         # Create a menu instance and attach it to root window
-        self.mbar = Menu(self)
-        self.master.config(menu = self.mbar)
+        self.mbar = tk.Menu(self)
+        self.master.config(menu=self.mbar)
         # Create a new menu instance and stick into the menu bar.
-        self.filemenu = Menu(self.mbar, tearoff = 0)
-        self.mbar.add_cascade(label = "File", menu = self.filemenu)
+        self.filemenu = tk.Menu(self.mbar, tearoff=0)
+        self.mbar.add_cascade(label="File", menu=self.filemenu)
         # Add entries to file menu.
-        self.filemenu.add_command(label = "Clear", command = self.clear)
-        self.filemenu.add_command(label = "Quit", command = self.main_quit)
+        self.filemenu.add_command(label="Clear", command=self.clear, accelerator="Ctrl+L")
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label="About", command=self.about, accelerator="Ctrl+A")
+        self.filemenu.add_command(label="Quit", command=self.main_quit, accelerator="Ctrl+Q")
+
+        # Keys bindings
+        self.master.bind("<Return>", self.key_return_event)         # Default action on Enter key
+        self.master.bind('<Control-q>', self.main_quit_event)       # Sedicated 2-arg event function
+        self.master.bind('<Control-l>', self.clear)                 # Shared event function with default value for event
+        self.master.bind('<Control-a>', lambda e: self.about())     # Relay-lambda to cann single arg event function
 
 
-        # Default action on Enter key
-        self.master.bind("<Return>", self.key_return_event)
+        tk.Label(self, text="Length in inches:").grid(padx=6, pady=6)
 
+        self.txt = tk.StringVar()
 
-        Label(self, text="Length in inches:").grid(padx=6, pady=6)
-
-        self.txt = StringVar()
-
-        self.ent = Entry(self, textvariable=self.txt, bg="Cyan", width=7)
+        self.ent = tk.Entry(self, textvariable=self.txt, bg="Cyan", width=7)
         self.ent.grid(row=0, column=1, padx=6, pady=6)
         self.ent.focus()
 
-        Button(self, text="Convert", command=self.btnCalc_click, borderwidth=3, relief=SOLID).grid(row=0, column=2, padx=6, pady=6)
+        # Convert button: Underline 1st letter and add a binding to Alt+C
+        tk.Button(self, text="Convert", underline=0, command=self.btnCalc_click, borderwidth=3, relief=tk.SOLID).grid(row=0, column=2, padx=6, pady=6)
+        self.master.bind('<Alt_L><c>', lambda e: self.btnCalc_click())
 
-        self.lblRes = Label(self, text='No value', fg="Blue")
+        self.lblRes = tk.Label(self, text='No value', fg="Blue")
         self.lblRes.grid(columnspan=3, padx=6, pady=6)
 
         self.btnQuit = MyQuitter(self)
@@ -63,16 +71,18 @@ class MyApp(Frame):
 
         # Intercept window direct closing
         self.master.protocol("WM_DELETE_WINDOW", self.btnQuit.myquit)
-    
 
     def main_quit(self):
         self.btnQuit.myquit()
 
-    
-    def clear(self):
+    # Version with event arg for shortcus binding
+    def main_quit_event(self, event):
+        self.main_quit()
+
+    # dual mode version, both for direct call and as an event handler
+    def clear(self, event=None):
         self.txt.set("")
         self.btnCalc_click()
-
 
     def btnCalc_click(self):
         v = self.txt.get()
@@ -80,17 +90,20 @@ class MyApp(Frame):
             out = 'No value'
         elif NUMBER_RE.fullmatch(v):
             cm = 2.54*float(v)
-            out = f"{cm} cm"
+            out = f"{cm:.2f} cm"
         else:
             out = "*** Err not numeric"
-            messagebox.showerror("in->cm", "Error: input is not numeric")            
+            tkmb.showerror("in->cm", "Error: input is not numeric")
 
         print(out)
         self.lblRes.configure(text=out)
 
-
     def key_return_event(self, event):
         self.btnCalc_click()
+    
+    def about(self):
+        tkmb.showinfo("in->cm", "My first tk application.\r\n© P.Violent 2018")
+
 
 if __name__ == "__main__":
     MyApp().mainloop()
