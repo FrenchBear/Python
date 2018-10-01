329a6712cc4a069f8fb7d5dbb8744b742cd7bef3
French Bear <FrenchBear38@outlook.com>
2018-10-01 16:40:34 +0200

    pylint: disable=assignment-from-no-return

--- Learning/55 Coroutines/coro.py
+++ Learning/55 Coroutines/coro.py
@@ -1,9 +1,14 @@
-# coroutines in Python
+# Coroutines in Python
 # Learning python
-# Totally empiric code after reading a mention of "yield from" and doing a google search
+# writer: Empiric code after reading a mention of "yield from" and doing a google search
 # https://stackoverflow.com/questions/9708902/in-practice-what-are-the-main-uses-for-the-new-yield-from-syntax-in-python-3
+# simple_coro: after reading pylint comments on error E1111
+#
+# But pylint (at least version 2.1.1) doesn't like to call a function that does not contain a return statement...
 #
 # 2018-09-28    PV
+
+# pylint: disable=assignment-from-no-return
 
 
 def writer():
@@ -18,13 +23,28 @@ def writer():
     print("writer ends")
 
 
-print("$1")
-w = writer()
-print("$2")
-w.send(None)  # "prime" the coroutine
-print("$3")
-for i in range(4):
-    w.send(i)
-print("$4")
-w.throw(StopIteration)
-print("$4")
+def test_writer():
+    print("$1")
+    w = writer()
+    print("$2")
+    w.send(None)  # "prime" the coroutine
+    print("$3")
+    for i in range(4):
+        w.send(i)
+    print("$4")
+    w.throw(StopIteration)
+    print("$4")
+
+
+import asyncio
+
+
+async def simple_coro():
+    await asyncio.sleep(0.1)
+    print("awake!")
+
+loop = asyncio.get_event_loop()
+coro1 = simple_coro()
+coro2 = simple_coro()
+loop.run_until_complete(asyncio.gather(coro1, coro2))
+loop.close()
