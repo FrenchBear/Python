bf66c54482c2b14bdfb4a3696b8541740eb56b40
French Bear <FrenchBear38@outlook.com>
2020-02-25 00:13:34 +0100

    Latest version

--- Learning/68 Rename TempBD/renBD1.py
+++ Learning/68 Rename TempBD/renBD1.py
@@ -22,6 +22,7 @@ def Step1(out):
         newname = ' '+normalize('NFC', basename)+' '
         newname = re.sub('\xa0', ' ', newname, flags=re.IGNORECASE)
         newname = re.sub(r'\.', ' ', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'–', '-', newname, flags=re.IGNORECASE)
         newname = re.sub(r'_', ' ', newname, flags=re.IGNORECASE)
         newname = re.sub(r'(( +-)|(- +))+', ' - ', newname, flags=re.IGNORECASE)
 
@@ -36,33 +37,44 @@ def Step1(out):
 
         # Renum T##
         def getnum(matchobj):
-            return ' '+(matchobj.group(1) if len(matchobj.group(1))==2 else '0'+matchobj.group(1))+' '
-        newname = re.sub(r' T(\d?\d) ', getnum, newname, flags=re.IGNORECASE)
+            n = int(matchobj.group(1))
+            return f' {n:0>2} '
+        newname = re.sub(r'T(\d?\d?\d)', getnum, newname, flags=re.IGNORECASE)
         newname = re.sub(r'Tome ', '', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'Tomo ', '', newname, flags=re.IGNORECASE)
 
         # Remove specific strings
         newname = re.sub(r'(BD|Comics|Manga)[ \.-]*FR', ' ', newname, flags=re.IGNORECASE)
-        newname = re.sub(r'Pressecitron|iBooker|FRENCH|HYBRiD|eBook|WEBRip|BDPACK|STC|REPACK|RESCAN|BROKER|One shot|MagPF|Full Color|Adult Comic', '', newname, flags=re.IGNORECASE)
-        newname = re.sub(r' OS | BD | BDX FR | BDX | FR | HD | HQ ', ' ', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'Pressecitron|iBooker|FRENCH|HYBRiD|eBook|WEBRip|BDPACK|STC|REPACK|RESCAN|BROKER|One shot|MagPF|Full Color|Adult Comic|Bdstudio|by 5Cobres', '', newname, flags=re.IGNORECASE)
+        newname = re.sub(r' OS | BDX FR | BDX | FR | HD | HQ | Pack ', ' ', newname, flags=re.IGNORECASE)
         newname = re.sub(r' !', '!', newname, flags=re.IGNORECASE)
         newname = re.sub(r'\d{4}px', '', newname, flags=re.IGNORECASE)
         newname = re.sub(r'1920|1600', '', newname, flags=re.IGNORECASE)
+
 
         # Replace wrong accents
         newname = re.sub(r'ÃÂ©', 'é', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã£Â¨', 'è', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã£Âª', 'ê', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'ÃÂ«', 'ë', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'ÃÂ´', 'ô', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'ÃÂª', 'ê', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'ÃÂ', 'à', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'Ã¹', 'ù', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã©', 'é', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã¨', 'è', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã´', 'ô', newname, flags=re.IGNORECASE)
         newname = re.sub(r"Ã'", 'ô', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã®', 'î', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'Ã«', 'ë', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã¯', 'ï', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ãª', 'ê', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã', 'â', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'Ã', 'à', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã¢', 'â', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã¤', 'ä', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã', 'é', newname, flags=re.IGNORECASE)
+        newname = re.sub(r'Ã§', 'ç', newname, flags=re.IGNORECASE)
         newname = re.sub(r'Ã ', 'à ', newname, flags=re.IGNORECASE)
         newname = re.sub(r'â', "'", newname, flags=re.IGNORECASE)
         newname = re.sub(r'â', "'", newname, flags=re.IGNORECASE)
