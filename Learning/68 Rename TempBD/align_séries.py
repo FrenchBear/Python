f118bf3887144b52d9a49bb6ce99769e82dfe358
French Bear <FrenchBear38@outlook.com>
2020-03-01 00:48:47 +0100

    save

--- Learning/68 Rename TempBD/align_séries.py
+++ Learning/68 Rename TempBD/align_séries.py
@@ -11,7 +11,7 @@ import re
 import unicodedata
 
 
-REBUILDFILESLIST = False
+REBUILDFILESLIST = True
 
 source = r'W:\TempBD'
 
@@ -31,10 +31,26 @@ else:
         files = json.load(infile)
 
 
+def memoize_normalize_serie(f): 
+    memory = {} 
+    # This inner function has access to memory and 'f' 
+    def inner(s): 
+        if s not in memory:          
+            memory[s] = f(s) 
+        return memory[s] 
+    return inner 
 
-def lower_noaccent(s: str) -> str:
+
+@memoize_normalize_serie
+def normalize_serie(serie: str) -> str:
     # Mn = Mark, Nonspacing = combining latin characters accents
-    return "".join(c for c in unicodedata.normalize("NFD", s.lower()) if unicodedata.category(c)!='Mn')       
+    serie = ''.join(c for c in unicodedata.normalize("NFD", serie.lower()) if unicodedata.category(c)!='Mn')       
+    for prefix in ['les aventures de ', 'une aventure de ', 'le ','la ', 'les ', "l'", 'un ', 'une ']:
+        if serie.startswith(prefix):
+            serie = serie[len(prefix):]
+    if serie.endswith(" pdf"):
+        serie = serie[:-4]
+    return serie
 
 print("Grouping files...")
 series: DefaultDict[str, set] = defaultdict(set)
@@ -45,7 +61,7 @@ for file in files:
     basename, ext = os.path.splitext(file)
     segments = basename.split(" - ")
     serie = segments[0]
-    serie_lna = lower_noaccent(serie)
+    serie_lna = normalize_serie(serie)
     series[serie_lna].add(serie)
 # Replace set by list to serialize
 # Or check https://stackoverflow.com/questions/8230315/how-to-json-serialize-sets
@@ -60,20 +76,29 @@ print(f'{len(series)} séries')
 
 def FindSeriesWithMultipleSpellings():
     print("Series with multiple spellings")
+    exceptions = [
+        ("Jojo", "Les aventures de Jojo"),
+        ("Le voyageur", "Voyageur"),
+        ("L'autre monde", "Un autre monde"),
+    ]
+    def isException(v):
+        return (v[0], v[1]) in exceptions
     seriesm: List[List[str]] = []
     with open(r'spellings.txt', mode='w', encoding='utf-8') as out:
         for (k,v) in series.items():
             if len(v)>1:
-                seriesm.append(list(v))
-                print(f'{k:<100} {len(v)}')
-                for spelling in v:
-                    out.write(spelling+';')
-                out.write('\n')
+                lv = sorted(list(v))
+                if not ((lv[0], lv[1]) in exceptions or (lv[1], lv[0]) in exceptions):
+                    seriesm.append(lv)
+                    print(f'{k:<100} {len(lv)}')
+                    for spelling in lv:
+                        out.write(spelling+';')
+                    out.write('\n')
     with open(r'spellings.json', 'w') as outfile:
         json.dump(seriesm, outfile, indent=4)
     print(f"Written {len(seriesm)} series with 2 or more spellings in spellings.txt and spellings.json")
 
-#FindSeriesWithMultipleSpellings()
+FindSeriesWithMultipleSpellings()
 
 
 def RenameSeriesUsingOfficialSpelling():
@@ -83,14 +108,14 @@ def RenameSeriesUsingOfficialSpelling():
         spo = json.load(infile)    
     dicspo = {}
     for spelling in spo:
-        dicspo[lower_noaccent(spelling)] = spelling
+        dicspo[normalize_serie(spelling)] = spelling
 
     # Rename series using official spelling
     for file in files:
         basename, ext = os.path.splitext(file)
         segments = basename.split(" - ")
         serie = segments[0]
-        serie_lna = lower_noaccent(serie)
+        serie_lna = normalize_serie(serie)
         if serie_lna in dicspo.keys():
             if segments[0] != dicspo[serie_lna]:
                 segments[0] = dicspo[serie_lna]
@@ -118,4 +143,4 @@ def FindSeriesEndingWithNumbers():
         outfile.write('\n'.join(sorted(snum)))
     print(f'{len(snum)} series written in seriesendnum.txt')
 
-FindSeriesEndingWithNumbers()
+#FindSeriesEndingWithNumbers()
