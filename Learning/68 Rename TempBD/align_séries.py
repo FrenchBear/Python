535d195f0222d942ac1c6750430f4445c306e019
French Bear <FrenchBear38@outlook.com>
2020-03-03 23:45:05 +0100

    save

--- Learning/68 Rename TempBD/align_séries.py
+++ Learning/68 Rename TempBD/align_séries.py
@@ -11,9 +11,9 @@ import re
 import unicodedata
 
 
-REBUILDFILESLIST = True
+REBUILDFILESLIST = False
 
-source = r'W:\TempBD'
+source = r'F:\TempBD'
 
 def GetFiles(source: str) -> List[str]:
     return list([f for f in os.listdir(source) if os.path.isfile(os.path.join(source, f))])
@@ -44,12 +44,18 @@ def memoize_normalize_serie(f):
 @memoize_normalize_serie
 def normalize_serie(serie: str) -> str:
     # Mn = Mark, Nonspacing = combining latin characters accents
-    serie = ''.join(c for c in unicodedata.normalize("NFD", serie.lower()) if unicodedata.category(c)!='Mn')       
-    for prefix in ['les aventures de ', 'une aventure de ', 'le ','la ', 'les ', "l'", 'un ', 'une ']:
+    serie = ''.join(c for c in unicodedata.normalize("NFD", serie.lower()) if unicodedata.category(c)!='Mn' and c!=',')
+    for prefix in ['les aventures de ', "les aventures d'", 'une aventure de ', "une aventure d'", "la legende de ", "la legende des ","la legende d'", "legende de ", "legende des ","legende d'", "legendes de ", "legendes des ","legendes d'", 'le ','la ', 'les ', "l'", 'un ', 'une ']:
         if serie.startswith(prefix):
             serie = serie[len(prefix):]
-    if serie.endswith(" pdf"):
-        serie = serie[:-4]
+    for suffix in [' - pdf', ' - rar', ' - zip',' pdf', ' rar', ' zip']:
+        if serie.endswith(suffix):
+            # A vérifier
+            serie = serie[:-len(suffix)]
+    for subst in [
+        ('blake & mortimer', 'blake et mortimer'),
+        ]:
+        serie = serie.replace(subst[0], subst[1])
     return serie
 
 print("Grouping files...")
@@ -74,7 +80,7 @@ print(f'{len(series)} séries')
 
 
 
-def FindSeriesWithMultipleSpellings():
+def find_series_with_multiple_spellings():
     print("Series with multiple spellings")
     exceptions = [
         ("Jojo", "Les aventures de Jojo"),
@@ -98,10 +104,10 @@ def FindSeriesWithMultipleSpellings():
         json.dump(seriesm, outfile, indent=4)
     print(f"Written {len(seriesm)} series with 2 or more spellings in spellings.txt and spellings.json")
 
-#FindSeriesWithMultipleSpellings()
+find_series_with_multiple_spellings()
 
 
-def RenameSeriesUsingOfficialSpelling():
+def rename_series_using_official_spelling():
     # Load official spellings
     spo = []
     with open(r'spellingsofficiel.json', 'r') as infile:
@@ -126,10 +132,10 @@ def RenameSeriesUsingOfficialSpelling():
                 except:
                     print("*** Err")
 
-RenameSeriesUsingOfficialSpelling()
+#rename_series_using_official_spelling()
 
 
-def FindSeriesEndingWithNumbers():
+def find_series_ending_with_numbers():
     print("Series ending with numbers")
     ENDDIGITS_RE = re.compile(r".*[ 0-9]+")
     with open(r'seriesvalidesavecnum.json', 'r') as infile:
@@ -143,4 +149,64 @@ def FindSeriesEndingWithNumbers():
         outfile.write('\n'.join(sorted(snum)))
     print(f'{len(snum)} series written in seriesendnum.txt')
 
-#FindSeriesEndingWithNumbers()
+#find_series_ending_with_numbers()
+
+
+
+
+diffMax = 1
+# Adapted from https://en.wikipedia.org/wiki/Levenshtein_distance and personal app DiffMP3Names
+def levenshtein_distance(s, t):
+    # Optimisation perso, j'ai pas besoin des distances supérieures à diffMax
+    if abs(len(s) - len(t)) > diffMax: return diffMax + 1
+
+    # degenerate cases
+    if s == t: return 0
+    if len(s) == 0: return len(t)
+    if len(t) == 0: return len(s)
+
+    # # convert to lowercase, we're doing case insensitive compare here
+    # s = s.lower()
+    # t = t.lower()
+
+    # create two work vectors of integer distances
+    # initialize v0 (the previous row of distances)
+    # this row is A[0][i]: edit distance for an empty s
+    # the distance is just the number of characters to delete from t
+    v0 = list(range(len(t)+1))
+    v1 = [0] * (len(t)+1)
+
+    for i in range(len(s)):
+        # calculate v1 (current row distances) from the previous row v0
+
+        # first element of v1 is A[i+1][0]
+        # edit distance is delete (i+1) chars from s to match empty t
+        v1[0] = i + 1
+
+        # use formula to fill in the rest of the row
+        for j in range(len(t)):
+            cost = 0 if s[i] == t[j] else 1
+            v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
+
+        # copy v1 (current row) to v0 (previous row) for next iteration
+        for j in range(len(v0)):
+            v0[j] = v1[j]
+
+    return v1[len(t)]
+
+
+def series_with_distance_one():
+    seriesnonum = []
+    for s in series.keys():
+        snonnum = re.sub(r' *[0-9·]+(-[0-9·]+)? *', ' ', s, flags=re.IGNORECASE).strip()
+        if s!=snonnum and len(snonnum)>0:
+            if not snonnum in seriesnonum:
+                seriesnonum.append(snonnum)
+            #print(f"{s:<80} <{snonnum}>")
+    
+    for i in range(len(seriesnonum)):
+        for j in range(i+1, len(seriesnonum)):
+            if levenshtein_distance(seriesnonum[i], seriesnonum[j])<=1:
+                print(f"{seriesnonum[i]:<80} {seriesnonum[j]}")
+
+#series_with_distance_one()
