e8f000b092ca182913d18e49ced4e05dfa44b5bb
French Bear <FrenchBear38@outlook.com>
2020-03-04 10:00:53 +0100

    copy_temp

---
+++ Learning/68 Rename TempBD/copy_temp_pair_impair.py
@@ -0,0 +1,23 @@
+import os, sys
+from typing import List, Iterable
+from shutil import copyfile
+
+
+source = r'\\teraz\backup_temp\temp'
+target = r'e:\eMuleTemp'
+
+def get_files(source: str) -> List[str]:
+    return list([f for f in os.listdir(source) if os.path.isfile(os.path.join(source, f))])
+
+def get_all_files(path: str) -> Iterable[str]:
+    for root, subs, files in os.walk(path):
+        for file in files:
+            yield os.path.join(root, file)
+
+for fullpath in get_all_files(source):
+    path, file = os.path.split(fullpath)
+    p = file.find('.')
+    n = int(file[:p])
+    if n%2==0:
+        print(n, file)
+        copyfile(fullpath, os.path.join(target, file))
