698d517aedf974e6a9349012af0a87e5d9bb129f
French Bear <FrenchBear38@outlook.com>
2020-09-26 12:48:48 +0200

    Added 03Pytha

---
+++ Learning/74 Puzzle Games/03Pytha.py
@@ -0,0 +1,24 @@
+# Pythagora Triangles Puzzles (§?)
+# Construire tous les triangles isocèles dont les sommets sont des noeuds (dont A (6,0))
+# et donc la longueur d'un des côtés est √34  
+# 2020-09-26    PV
+
+import math
+import itertools
+
+noeuds = []
+for x in range(7):
+    for y in range(7):
+        if (x,y)!=(6,0):
+            noeuds.append((x,y))
+
+C = (6,0)
+r34 = math.sqrt(34)
+for (A,B) in itertools.combinations(noeuds, 2):
+    dAB = math.dist(A,B)
+    dAC = math.dist(A,C)
+    dBC = math.dist(B,C)
+    if math.isclose(dAB, dAC) or math.isclose(dAB, dBC) or math.isclose(dAC, dBC):
+        if math.isclose(dAB, r34) or math.isclose(dAC, r34) or math.isclose(dBC,r34):
+            print(A,B,C)
+ 
