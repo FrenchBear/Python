7020de9e5cb04319b6503d9879d6de49a9847c9f
French Bear <FrenchBear38@outlook.com>
2021-04-30 01:04:26 +0200

    end of Tombola interface

---
+++ Learning/83 ABC/tombolist.py
@@ -0,0 +1,28 @@
+# tobolist is a class implementing Tombola interface and inheriting from list, but using virtual inheritance
+# 2021-04-28    PV
+
+from random import randrange
+from tombola import Tombola
+from typing import Any, Iterable
+
+@Tombola.register               # class registered as a virtual subclass of Tombola
+class TomboList(list):
+    def pick(self):
+        if (self):
+            return self.pop(randrange(len(self)))
+        raise LookupError('pop from empty ToboList')
+    
+    load = list.extend
+
+    def loaded(self):
+        return bool(self)       # Can't use trick of previous line since list does not implement __bool__
+
+    def inspect(self):
+        return tuple(sorted(self))
+
+
+if __name__=='__main__':
+    tl = TomboList()
+    print(issubclass(TomboList, Tombola))
+    print(isinstance(tl, Tombola))
+    print(TomboList.__mro__)        # No fallback on Tombola since TomboList doesn't inherit from Tombola
