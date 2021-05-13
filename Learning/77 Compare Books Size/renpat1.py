6581c83142658d4097bff918a24f7814d1371154
French Bear <FrenchBear38@outlook.com>
2021-05-13 02:31:50 +0200

    Update renpat1

--- Learning/77 Compare Books Size/renpat1.py
+++ Learning/77 Compare Books Size/renpat1.py
@@ -2,13 +2,14 @@
 # reformat book names such as
 # 01 castellia memotech plus electrotechnique.pdf
 # 2021-01-14    PV
+# 2021-05-13    PV      Filtre .pdf
 
 from vlib import *
 from typing import Dict
 import os, shutil
 import re
 
-source = r'D:\Downloads\A_Trier\!A_Trier_Livres\Pat1'
+source = r'D:\Downloads\A_Trier\!A_Trier_Livres'
 
 ED = re.compile(r" (\d+)ed$")
 
@@ -21,10 +22,11 @@ def added(s:str) -> str:
     return s
 
 for file in list(get_files(source)):
-    basename, ext = os.path.splitext(file)
-    ts = basename.split(' ')
-    editeur = ts[1]
-    newname = added(' '.join(ts[2:])) + ' - [' + editeur.title() + '] - X.pdf'
-    newname = newname[0].upper() + newname[1:]
-    print('-> ' + newname)
-    os.rename(os.path.join(source, file), os.path.join(source, newname))
+    if file.lower().endswith('.pdf'):
+        basename, ext = os.path.splitext(file)
+        ts = basename.split(' ')
+        editeur = ts[1]
+        newname = added(' '.join(ts[2:])) + ' - [' + editeur.title() + '] - X.pdf'
+        newname = newname[0].upper() + newname[1:]
+        print('-> ' + newname)
+        os.rename(os.path.join(source, file), os.path.join(source, newname))
