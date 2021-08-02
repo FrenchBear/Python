e513a31d88696c19c094432f7e96b4ee2e367dd8
FrenchBear38 (Mac) <frenchbear38@outlook.com>
2021-08-02 22:20:23 +0200

    Finished Pipe class

--- Learning/89 Pipe/pipe.py
+++ Learning/89 Pipe/pipe.py
@@ -1,14 +1,20 @@
 # pipe.py
-# play with pipeline architecture
+# Play with pipeline model to chain transformations such as filtering or sorting easily
+# No attempt is made to curcumvent the Python issue that an iterator can only be used once.
+#
+# ToDo later: implement group_by (not difficult)
+#
 # 2021-08-01    PV
 
-from typing import Callable, Iterable, Iterator, Optional, TypeVar
+from typing import Callable, Iterable, Iterator, Optional, TypeVar, cast, Any
 from numbers import Number
 
 T = TypeVar('T')
 U = TypeVar('U')
 
-
+# A wrapper for iterable structure.  Member methods such as where or sort returns the output as
+# a Pipe, enabling convenient chaining until (optional) final transformation that does not return
+# a Pipe.
 class Pipe:
     def __init__(self, source: Iterable[T]) -> None:
         # Will raise a TypeError if source is not iterable
@@ -17,47 +23,188 @@ class Pipe:
     def __iter__(self) -> Iterator[T]:
         return self.iter
 
-    def pipe_where(self, predicate: Callable[[T], bool]) -> 'Pipe':
+    # Also used for str()
+    def __repr__(self) -> str:
+        l = []
+        i = 0
+        for item in self.iter:
+            i += 1
+            if i > 15:
+                l.append('...')
+                break
+            l.append(repr(item))
+        return '['+','.join(l)+']'
+
+
+    # --------------------------------
+    # Static methods of pipe (generators returning a pipe)
+
+    @staticmethod
+    def range(start: int, stop: Optional[int] = None, step: int = 1) -> 'Pipe':
+        """Returns a pipe of integers using same parameters as Python builtin range([start=0], end, [step=1]), that is the sequence from start to end-1 by step."""
+        if stop is None:
+            start, stop = 0, start
+        return Pipe(range(start, stop, step))
+
+    @staticmethod
+    def repeat(element: T, count: int) -> 'Pipe':
+        """Returns an pipe containing element repeated count times"""
+        def repeater():
+            for i in range(count):
+                yield element
+        return Pipe(repeater())
+
+
+    # --------------------------------
+    # Tranformation functions of a pipe (returning a pipe)
+
+    def where(self, predicate: Callable[[T], bool]) -> 'Pipe':
+        """Filters elements of the pipe, only keeping the ones matching the preicate provided."""
         def iter_where():
             for item in self.iter:
                 if predicate(item):
                     yield item
         return Pipe(iter_where())
 
-    def pipe_select(self, select: Callable[[T], U]) -> 'Pipe':
+    def select(self, select: Callable[[T], U]) -> 'Pipe':
+        """Trasforms elements of the pipe using function provided."""
         def iter_select():
             for item in self.iter:
                 yield select(item)
         return Pipe(iter_select())
 
-    def pipe_first(self, predicate: Optional[Callable[[T], bool]] = None) -> 'Pipe':
-        def first_or_none():
-            for item in self.iter:
-                if predicate:
-                    if predicate(item):
-                        yield item
-                        return
-                else:
-                    yield item
-                    return
-            # Do not yield anything if there is no match for a first
-        return Pipe(first_or_none())
-
-    def pipe_sort(self, key = None, reverse: bool = False) -> 'Pipe':
+    def sort(self, key=None, reverse: bool = False) -> 'Pipe':
+        """Sorts elements of the pipe. If no key selector is provided, sorts on elements value. reverse argument reverses the sort."""
         def sorter():
             l = list(self.iter)
             l.sort(key=key, reverse=reverse)
             yield from l
         return Pipe(sorter())
 
-    def pipe_reverse(self) -> 'Pipe':
+    def reverse(self) -> 'Pipe':
+        """Reverses elements of the pipe."""
         def reverser():
             l = list(self.iter)
             l.reverse()
             yield from l
         return Pipe(reverser())
 
-    def final_count(self, predicate: Optional[Callable[[T], bool]] = None) -> int:
+    def concat(self, *iterables) -> 'Pipe':
+        """Concatenates iterables to current pipe, keeping duplicates."""
+        def concatener():
+            yield from self
+            for iterable in iterables:
+                yield from iterable
+        return Pipe(concatener())
+
+    def distinct(self) -> 'Pipe':
+        """Remove duplicate elements in the pipe."""
+        def deduplicator():
+            s = set(self.iter)
+            yield from s
+        return Pipe(deduplicator())
+
+    def intersect(self, *iterables) -> 'Pipe':
+        """Produces the pipe intersection of enumerables."""
+        def intersector():
+            s = set(self.iter).intersection(*iterables)
+            yield from s
+        return Pipe(intersector())
+
+    def union(self, *iterables) -> 'Pipe':
+        """Produces the pipe union of enumerables, removing duplicates."""
+        def unionor():
+            s = set(self.iter).union(*iterables)
+            yield from s
+        return Pipe(unionor())
+
+    def skip(self, count: int) -> 'Pipe':
+        """Bypasses a specified number of elements in a pipe and then returns the remaining elements."""
+        def skipper():
+            nonlocal count
+            for item in self.iter:
+                if count>0:
+                    count-=1
+                else:
+                    yield item
+        return Pipe(skipper())
+
+    def skip_last(self, count: int) -> 'Pipe':
+        """Returns a new pipe that contains the elements from source with the last count elements omitted."""
+        def skipper_last():
+            l = list(self.iter)
+            yield from l[:-count]
+        return Pipe(skipper_last())
+
+    def take(self, count: int) -> 'Pipe':
+        """Returns a specified number of contiguous elements from the start of a pipe."""
+        def taker():
+            nonlocal count
+            for item in self.iter:
+                if count>0:
+                    yield item
+                    count-=1
+                else:
+                    break
+        return Pipe(taker())
+
+    def take_last(self, count: int) -> 'Pipe':
+        """Returns a new pipe that contains the last count elements from source."""
+        def taker_last():
+            l = list(self.iter)
+            yield from l[-count:]
+        return Pipe(taker_last())
+
+    def take_while(self, predicate: Callable[[T], bool]) -> 'Pipe':
+        """Returns elements from a sequence as long as a specified condition is true, and then skips the remaining elements."""
+        def taker_while():
+            for item in self.iter:
+                if not predicate(item):
+                    break
+                yield item
+        return Pipe(taker_while())
+
+    def skip_while(self, predicate: Callable[[T], bool]) -> 'Pipe':
+        """Bypasses elements in a pipe as long as a specified condition is true and then returns the remaining elements."""
+        def skipper_while():
+            do_yield = False
+            for item in self.iter:
+                if do_yield:
+                    yield item
+                else:
+                    if not predicate(item):
+                        do_yield = True
+                        yield item
+        return Pipe(skipper_while())
+
+    def zip(self, *iterables) -> 'Pipe':
+        """Produces a pipe of tuples with elements from the specified sequences. Stops at the end of shortest pipe."""
+        def zipper():
+            z = zip(self.iter, *iterables)
+            yield from z
+        return Pipe(zipper())
+
+    def append(self, value: T) -> 'Pipe':
+        """Adds a value to the end of the pipe."""
+        def appender():
+            yield from self.iter
+            yield value
+        return Pipe(appender())
+
+    def prepend(self, value: T) -> 'Pipe':
+        """Adds a value to the beginning of the pipe."""
+        def prepender():
+            yield value
+            yield from self.iter
+        return Pipe(prepender())
+
+
+    # --------------------------------
+    # Immediate (final) functions of a pipe (not returning a pipe)
+
+    def count(self, predicate: Optional[Callable[[T], bool]] = None) -> int:
+        """Returns number of elements of the pipe, or count only elements matching the predicate if it is provided.
+        Immediate execution on call."""
         count = 0
         for item in self.iter:
             if predicate:
@@ -67,7 +214,32 @@ class Pipe:
                 count += 1
         return count
 
-    def final_first_or_none(self, predicate: Optional[Callable[[T], bool]] = None) -> T:
+    def aggregate(self, aggregator: Callable, predicate: Optional[Callable[[T], bool]] = None) -> Any:
+        """Aggregates a pipe of values, or only elements matching the predicate if it is provided, using aggregator.
+        Immediate execution on call."""
+        if predicate:
+            return aggregator(item for item in self.iter if predicate(item))
+        else:
+            return aggregator(self.iter)
+
+    def sum(self, predicate: Optional[Callable[[float], bool]] = None) -> float:
+        """Sums all elements of a pipe of numbers, or only elements matching the predicate if it is provided.
+        Immediate execution on call."""
+        return self.aggregate(sum, predicate)
+
+    def min(self, predicate: Optional[Callable[[T], bool]] = None) -> Any:
+        """Returns min of a pipe of numbers, or only elements matching the predicate if it is provided.
+        Immediate execution on call."""
+        return self.aggregate(min, predicate)
+
+    def max(self, predicate: Optional[Callable[[T], bool]] = None) -> Any:
+        """Returns min of a pipe of numbers, or only elements matching the predicate if it is provided.
+        Immediate execution on call."""
+        return self.aggregate(max, predicate)
+
+    def first_or_none(self, predicate: Optional[Callable[[T], bool]] = None) -> Optional[T]:
+        """Returns first element of the pipe, or the first element matching the predicate if it is provided. If pipe is empty or no matching element found, returns None.
+        Immediate execution on call."""
         for item in self.iter:
             if predicate:
                 if predicate(item):
@@ -77,21 +249,67 @@ class Pipe:
         # Return None if there is no match for a first
         return None
 
-    def final_sum(self, predicate: Optional[Callable[[T], bool]] = None) -> Number:
-        sum:Number = 0
+    def last_or_none(self, predicate: Optional[Callable[[T], bool]] = None) -> Optional[T]:
+        """Returns last element of the pipe, or the last element matching the predicate if it is provided. If pipe is empty or no matching element found, returns None.
+        Immediate execution on call."""
+        last = None
         for item in self.iter:
             if predicate:
                 if predicate(item):
-                    sum += item
+                    last = item
             else:
-                sum += item
-        return sum
+                last = item
+        return last
+
+    def join(self, separator: str, predicate: Optional[Callable[[str], bool]] = None) -> str:
+        """Joins all elements of a pipe of strings into a string using separator, or only elements matching the predicate if it is provided.
+        Immediate execution on call."""
+        if predicate:
+            return separator.join(item for item in cast(Iterator[str], self.iter) if predicate(item))
+        else:
+            return separator.join(cast(Iterator[str], self.iter))
+
+    def any(self, predicate: Optional[Callable[[T], bool]] = None) -> bool:
+        """Without a predicate, checks if pipe contains at least one element.  With a predicate, checks if the pipe contains at least one element that matches the predicate.
+        Immediate execution on call."""
+        if predicate:
+            for item in self.iter:
+                if predicate(item):
+                    return True
+            return False
+        else:
+            try:
+                _ = next(self.iter)
+            except StopIteration:
+                return False
+            return True
+
+    def all(self, predicate: Callable[[T], bool]) -> bool:
+        """Checks if all elements of the pipe match the predicate. If the pipe is empty, returns True.
+        Immediate execution on call."""
+        for item in self.iter:
+            if not predicate(item):
+                return False
+        return True
+
+    def contains(self, value: T) -> bool:
+        """Checks if the pipe contains value provided.
+        Immediate execution on call."""
+        for item in self.iter:
+            if item==value:
+                return True
+        return False
 
 
 
-animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+# animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+# fruits = ['pomme', 'poire', 'ananas']
+# couleurs = ['bleu', 'blanc', 'rouge']
 
-p = Pipe(animaux).pipe_reverse()
-r = ','.join(repr(a) for a in p)
-print(f'[{r}]')
-
+#p = Pipe(animaux).concat(fruits, couleurs)
+#print(p)
+#print(Pipe.range(5).contains(9))
+#print(Pipe(fruits).contains('poire'))
+#print(Pipe([1,2,3]).any())
+#print(Pipe([1,2,3]).any(lambda a: a==2))
+#print(Pipe([1,2,3]).any(lambda a: a==5))
--- Learning/89 Pipe/test_pipe.py
+++ Learning/89 Pipe/test_pipe.py
@@ -9,110 +9,253 @@ import unittest
 class TestPipe(unittest.TestCase):
 
     # A Pipe(it) is iterable and iterates over argument it
-    def test_pipe_1(self):
+    def test_1(self):
         p = Pipe([1, 2, 3])
         self.assertEqual(list(p), [1, 2, 3])
 
     # Calling Pipe() with non-iterable arg raises a TypeError
-    def test_pipe_2(self):
+    def test_2(self):
         self.assertRaises(TypeError, Pipe, 23)
 
     # where filtering
     def test_where_1(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_where(lambda a: a.startswith('c'))
+        p = Pipe(animaux).where(lambda a: a.startswith('c'))
         self.assertEqual(list(p), ['chien', 'chat'])
 
     def test_where_2(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_where(lambda a: a.startswith('z'))
+        p = Pipe(animaux).where(lambda a: a.startswith('z'))
         self.assertEqual(list(p), [])
 
     # select transformation
     def test_select_1(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_select(lambda a: len(a))
+        p = Pipe(animaux).select(lambda a: len(a))
         self.assertEqual(list(p), [5, 4, 4, 3, 4, 5])
 
-    # first selection
+    # select first
     def test_first_1(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_first()
-        self.assertEqual(list(p), ['chien'])
+        p = Pipe(animaux).first_or_none()
+        self.assertEqual(p, 'chien')
 
     def test_first_2(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_first(lambda a: a.startswith('o'))
-        self.assertEqual(list(p), ['ours'])
+        p = Pipe(animaux).first_or_none(lambda a: a.startswith('r'))
+        self.assertEqual(p, 'rat')
 
     def test_first_3(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_first(lambda a: a.startswith('z'))
-        self.assertEqual(list(p), [])
+        p = Pipe(animaux).first_or_none(lambda a: len(a) == 20)
+        self.assertEqual(p, None)
 
-    def test_first_4(self):
+    # select last
+    def test_last_1(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).final_first_or_none()
-        self.assertEqual(p, 'chien')
+        p = Pipe(animaux).last_or_none()
+        self.assertEqual(p, 'boeuf')
 
-    def test_first_5(self):
+    def test_last_2(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).final_first_or_none(lambda a: a.startswith('r'))
-        self.assertEqual(p, 'rat')
+        p = Pipe(animaux).last_or_none(lambda a: a.startswith('c'))
+        self.assertEqual(p, 'chat')
 
-    def test_first_6(self):
+    def test_last_3(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).final_first_or_none(lambda a: len(a) == 20)
+        p = Pipe(animaux).last_or_none(lambda a: len(a) == 20)
         self.assertEqual(p, None)
 
     # count elements
     def test_count_1(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).final_count()
+        p = Pipe(animaux).count()
         self.assertEqual(p, 6)
 
     def test_count_2(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).final_count(lambda a: len(a) == 4)
+        p = Pipe(animaux).count(lambda a: len(a) == 4)
         self.assertEqual(p, 3)
 
     # sum elements
     def test_sum_1(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_select(lambda a: len(a)).final_sum()
+        p = Pipe(animaux).select(lambda a: len(a)).sum()
         self.assertEqual(p, 25)
 
     def test_sum_2(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_select(lambda a: len(a)).final_sum(lambda a: a > 4)
+        p = Pipe(animaux).select(lambda a: len(a)).sum(lambda a: a > 4)
         self.assertEqual(p, 10)
+
+    def test_sum_3(self):
+        p = Pipe(range(1, 10, 2)).sum()
+        self.assertEqual(p, 25)     # 25 = 1+3+5+7+9
+
+    # Min
+    def test_min_1(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).min()
+        self.assertEqual(p, 'boeuf')
+
+    def test_min_2(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).min(lambda a: len(a)==4)
+        self.assertEqual(p, 'chat')
+
+    # Max
+    def test_max_1(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).max()
+        self.assertEqual(p, 'rat')
+
+    def test_max_2(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).max(lambda a: len(a)==4)
+        self.assertEqual(p, 'porc')
 
     # sort elements
     def test_sort_1(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_sort()
+        p = Pipe(animaux).sort()
         self.assertEqual(list(p), ['boeuf', 'chat', 'chien', 'ours', 'porc', 'rat'])
 
     def test_sort_2(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_sort(reverse=True)
-        self.assertEqual(list(p), ['rat','porc','ours','chien','chat','boeuf'])
+        p = Pipe(animaux).sort(reverse=True)
+        self.assertEqual(list(p), ['rat', 'porc', 'ours', 'chien', 'chat', 'boeuf'])
 
     def test_sort_3(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_sort(lambda a: len(a))
-        self.assertEqual(list(p), ['rat','chat','ours','porc','chien','boeuf'])
+        p = Pipe(animaux).sort(lambda a: len(a))
+        self.assertEqual(list(p), ['rat', 'chat', 'ours', 'porc', 'chien', 'boeuf'])
 
     def test_sort_2(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_sort(lambda a: len(a), True)
-        self.assertEqual(list(p), ['chien','boeuf','chat','ours','porc','rat'])
+        p = Pipe(animaux).sort(lambda a: len(a), True)
+        self.assertEqual(list(p), ['chien', 'boeuf', 'chat', 'ours', 'porc', 'rat'])
 
     # reverse elements
-    def test_reverse_1(self):
+    def test_reverse(self):
         animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
-        p = Pipe(animaux).pipe_reverse()
-        self.assertEqual(list(p), ['boeuf','porc','rat','ours','chat','chien'])
+        p = Pipe(animaux).reverse()
+        self.assertEqual(list(p), ['boeuf', 'porc', 'rat', 'ours', 'chat', 'chien'])
+
+    # contat iterables
+    def test_concat(self):
+        animaux = ['chien', 'chat', 'ours']
+        fruits = ['pomme', 'poire', 'ananas']
+        couleurs = ['bleu', 'blanc', 'rouge']
+        p = Pipe(animaux).concat(fruits, couleurs)
+        self.assertEqual(list(p), ['chien', 'chat', 'ours', 'pomme', 'poire', 'ananas', 'bleu', 'blanc', 'rouge'])
+
+    # join elements
+    def test_join(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).join(',')
+        self.assertEqual(p, 'chien,chat,ours,rat,porc,boeuf')
+
+    # range generator
+    def test_range(self):
+        self.assertEqual(list(Pipe.range(10)), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.assertEqual(list(Pipe.range(1, 10)), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+        self.assertEqual(list(Pipe.range(1, 10, 2)), [1, 3, 5, 7, 9])
+
+    # test any()
+    def test_any(self):
+        self.assertEqual(Pipe([]).any(), False)
+        self.assertEqual(Pipe([1, 2, 3]).any(), True)
+        self.assertEqual(Pipe([1, 2, 3]).any(lambda a: a == 2), True)
+        self.assertEqual(Pipe([1, 2, 3]).any(lambda a: a == 5), False)
+
+    # test all()
+    def test_all(self):
+        self.assertEqual(Pipe.range(5).all(lambda x: x >= 0), True)
+        self.assertEqual(Pipe.range(5).all(lambda x: x >= 6), False)
+
+    # test contains()
+    def test_contains(self):
+        fruits = ['pomme', 'poire', 'ananas']
+        self.assertEqual(Pipe.range(5).contains(9), False)
+        self.assertEqual(Pipe(fruits).contains('poire'), True)
+
+    # test distinct()
+    def test_distinct(self):
+        p = Pipe('mississippi').distinct().sort()
+        self.assertEqual(list(p), ['i', 'm', 'p', 's'])
+
+    # test intersect()
+    def test_intersect(self):
+        p = Pipe.range(0, 100, 2).intersect(Pipe.range(0, 100, 3), Pipe.range(0, 100, 5)).sort()
+        self.assertEqual(list(p), [0, 30, 60, 90])
+    
+    # test union()
+    def test_union(self):
+        p = Pipe('asterix').union(Pipe('obelix')).sort()
+        self.assertEqual(list(p), ['a','b','e','i','l','o','r','s','t','x'])
+
+    # test repeat()
+    def test_repeat(self):
+        p = Pipe.repeat('yes!', 3)
+        self.assertEqual(list(p), ['yes!', 'yes!', 'yes!'])
+        p = Pipe.repeat(1, 6)
+        self.assertEqual(list(p), [1, 1, 1, 1, 1, 1])
+
+    # test_skip()
+    def test_skip(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).skip(2)
+        self.assertEqual(list(p), ['ours','rat','porc','boeuf'])
+
+    # test skip_last()
+    def test_skip_last(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).skip_last(2)
+        self.assertEqual(list(p), ['chien', 'chat', 'ours', 'rat'])
+
+    # test take()
+    def test_take(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).take(2)
+        self.assertEqual(list(p), ['chien', 'chat'])
+
+    # test take_last()
+    def test_take_last(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).take_last(2)
+        self.assertEqual(list(p), ['porc', 'boeuf'])
+    
+    # test skip_while()
+    def test_take_while(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).take_while(lambda a: a!='ours')
+        self.assertEqual(list(p), ['chien', 'chat'])
+
+    # test skip_while()
+    def test_skip_while(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).skip_while(lambda a: a!='ours')
+        self.assertEqual(list(p), ['ours','rat','porc','boeuf'])
+
+    # test zip()
+    def test_zip(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        fruits = ['pomme', 'poire', 'ananas']
+        couleurs = ['bleu', 'blanc', 'rouge']
+        p = Pipe(animaux).zip(fruits, couleurs)
+        self.assertEqual(list(p), [('chien', 'pomme', 'bleu'),('chat', 'poire', 'blanc'),('ours', 'ananas', 'rouge')])
+
+    # test append()
+    def test_append(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).append('lion')
+        self.assertEqual(list(p), ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf', 'lion'])
+
+    # test prepend()
+    def test_prepend(self):
+        animaux = ['chien', 'chat', 'ours', 'rat', 'porc', 'boeuf']
+        p = Pipe(animaux).prepend('lion')
+        self.assertEqual(list(p), ['lion', 'chien', 'chat', 'ours', 'rat', 'porc', 'boeuf'])
 
 
 
