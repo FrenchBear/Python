1121aa2382229b31962c8c5b14377425f9e8593d
French Bear <FrenchBear38@outlook.com>
2021-11-03 11:21:49 +0100

    fraction development using almost no memory

--- Learning/93_Fraction_Development/fd1.py
+++ Learning/93_Fraction_Development/fd1.py
@@ -1,5 +1,5 @@
-# Fraction Development
-# 2021-10-23    PV      First version with dictionary and string
+# Fraction Development 1
+# 2021-10-23    PV      First version with dictionary and string, O(d) memory and O(d) time
 
 # ToDo: sign, /0
 def develop(n: int, d:int) -> str:
@@ -24,6 +24,8 @@ def develop(n: int, d:int) -> str:
 
     return ''
 
+#print(develop(1,9801))
 # Decimals .[00 01 02 03 ... 96 97 99]
 # 0.[000102030405060708091011121314151617181920212223242526272829303132333435363738394041424344454647484950515253545556575859606162636465666768697071727374757677787980818283848586878889909192939495969799]
-print(develop(1,9801))
+
+print(develop(100,23))
--- Learning/93_Fraction_Development/fd2.py
+++ Learning/93_Fraction_Development/fd2.py
@@ -1,29 +1,47 @@
-# Fraction Development
-# 2021-10-23    PV      First version with dictionary and string
+# Fraction Development 2
+# 2021-11-03    PV      Second version using O(1) memory (sint does not really count) but O(d²) time
 
-# ToDo: sign, /0
-def develop(n: int, d:int) -> str:
-    dic: dict[int, int] = {}
+def develop2(n: int, d: int) -> str:
     sint = str(n//d) + '.'
     n %= d
-    sfrac = ''
+
+    n0 = n
+    k = 0
     for i in range(d):
         n *= 10
         dec = n//d
         n %= d
 
-        if n==0:    # division ends
-            return sint+sfrac
+        if n == 0:    # division ends
+            k = 1
+            break
 
-        if n in dic:    # found period
-            return sint+sfrac[:dic[n]]+'['+sfrac[dic[n]:]+']'
-        
-        # record position for reminder and decimal, and continue to next decimal
-        dic[n] = i
-        sfrac += str(dec)
+        m = n0
+        for j in range(i):
+            m *= 10
+            decl = m//d
+            m %= d
+            if n == m:
+                # Found a repeating remainder, it's periodic
+                k = 2
+                break
+        if k > 0:
+            break
 
-    return ''
+    n = n0
+    if k == 2:
+        sint += "["
+    for i in range(d):
+        n *= 10
+        dec = n//d
+        n %= d
+        if n == m and i>0:
+            return sint+']'
+        sint += str(dec)
+        if n == 0:
+            return sint
+
 
 # Decimals .[00 01 02 03 ... 96 97 99]
 # 0.[000102030405060708091011121314151617181920212223242526272829303132333435363738394041424344454647484950515253545556575859606162636465666768697071727374757677787980818283848586878889909192939495969799]
-print(develop(1,9801))
+print(develop2(100, 23))
