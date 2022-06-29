84304e097ba8a63f89a8abd17e0c2fdfa12cf4ba
French Bear <FrenchBear38@outlook.com>
2022-06-30 00:22:59 +0200

    save

--- Learning/077_Compare_Books_Size/renpat1.py
+++ Learning/077_Compare_Books_Size/renpat1.py
@@ -1,6 +1,7 @@
 # renpat1.py
 # reformat book names such as
 # 01 castellia memotech plus electrotechnique.pdf
+#
 # 2021-01-14    PV
 # 2021-05-13    PV      Filtre .pdf
 
@@ -9,7 +10,8 @@ from typing import Dict
 import os, shutil
 import re
 
-source = r'D:\Downloads\A_Trier\!A_Trier_Livres'
+source = r'D:\Downloads\A_Trier\!A_Trier'
+doit = False
 
 ED = re.compile(r" (\d+)ed$")
 
@@ -29,4 +31,6 @@ for file in list(get_files(source)):
         newname = added(' '.join(ts[2:])) + ' - [' + editeur.title() + '] - X.pdf'
         newname = newname[0].upper() + newname[1:]
         print('-> ' + newname)
-        os.rename(os.path.join(source, file), os.path.join(source, newname))
+        if doit:
+            os.rename(os.path.join(source, file), os.path.join(source, newname))
+            
