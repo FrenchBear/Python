b8e8ab1bbd9ccf59b86b9b192eaeac2fbddde93d
French Bear <FrenchBear38@outlook.com>
2022-07-05 07:06:50 +0200

    save

---
+++ Learning/clean.ps1
@@ -0,0 +1,25 @@
+﻿# clean.ps1
+# Delete __pycache__ and .mypy_cache in every python project
+#
+# 2022-07-05  PV
+
+$root = 'C:\Development\GitHub\Python\Learning'
+foreach($d in Get-ChildItem -Directory -Path $root)
+{
+    Write-Host($d)
+    if (Test-Path -Path "$($d.FullName)\__pycache__") {
+        Remove-Item -Recurse -Force "$($d.FullName)\__pycache__"
+    }
+    if (Test-Path -Path "$($d.FullName)\.mypy_cache") {
+        Remove-Item -Recurse -Force "$($d.FullName)\.mypy_cache"
+    }
+    if (Test-Path -Path "$($d.FullName)\.idea") {
+        Remove-Item -Recurse -Force "$($d.FullName)\.idea"
+    }
+    if (Test-Path -Path "$($d.FullName)\.vs") {
+        Remove-Item -Recurse -Force "$($d.FullName)\.vs"
+    }
+    if (Test-Path -Path "$($d.FullName)\.vscode\.ropeproject") {
+        Remove-Item -Recurse -Force "$($d.FullName)\.vscode\.ropeproject"
+    }
+}
