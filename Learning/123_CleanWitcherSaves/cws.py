6ecf23c51a87f1e942056719c9c3a01468bf80dd
French Bear <FrenchBear38@outlook.com>
2023-02-20 00:25:30 +0100

    save

--- Learning/123_CleanWitcherSaves/cws.py
+++ Learning/123_CleanWitcherSaves/cws.py
@@ -19,18 +19,28 @@ for file in get_files(source):
         mt = datetime.fromtimestamp(os.stat(filefp).st_mtime)  # , tz=timezone.utc)
         dicTimeFile[mt] = filefp
 
+nskip = 0
+nkeep = 0
+ndel = 0
 last = datetime.today() + timedelta(days=1)     # Fake last time kept, somewhere in the future
 sortedTime = sorted(dicTimeFile.keys(), reverse=True)
 start = sortedTime[0]-timedelta(days=1)     # From the last save, keep everything for the last 24 hours
 for t in sortedTime:
     if t > start:
         status = 'Skip'
+        nskip += 1
     elif last-t > spacemin:
         status = 'Keep'
+        nkeep += 1
         last = t
     else:
         status = 'Delete'
+        ndel += 1
         if doit:
             os.remove(dicTimeFile[t])
             os.remove(dicTimeFile[t].replace('.sav', '.png'))
     print(f'{t:%Y-%m-%d %H:%M:%S}  {dicTimeFile[t]}  {status}')
+
+if not doit:
+    print('No action mode')
+print('Skip:', nskip, '  Kept:', nkeep, '  Deleted:', ndel)
