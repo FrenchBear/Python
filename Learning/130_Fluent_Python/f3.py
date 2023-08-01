3aab71a1ae8dce7844291cef6792a0c392360b2e
French Bear <FrenchBear38@outlook.com>
2023-08-01 16:26:03 +0200

    save

--- Learning/130_Fluent_Python/f3.py
+++ Learning/130_Fluent_Python/f3.py
@@ -1,22 +1,49 @@
-from collections.abc import Callable
+import time
+import functools
 
-def update(
-        probe: Callable[[], float],
-        display: Callable[[float], None]
-    ) -> None:
-    temperature = probe()
-    # imagine lots of control code here
-    display(temperature)
+DEFAULT_FMT = '[{elapsed:0.8f}s] {name}({args}) -> {result}'
 
-def probe_ok() -> int:
-    return 42
+def clock(fmt=DEFAULT_FMT):
+    def decorate(func):
+        @functools.wraps(func)
+        def clocked(*_args):
+            t0 = time.perf_counter()
+            _result = func(*_args)
+            elapsed = time.perf_counter() - t0
+            name = func.__name__
+            args = ', '.join(repr(arg) for arg in _args)
+            result = repr(_result)
+            print(fmt.format(**locals()))
+            return _result
+        return clocked
+    return decorate
 
-def display_wrong(temperature: int) -> None:
-    print(hex(temperature))
+class clock2:
+    def __init__(self, fmt=DEFAULT_FMT):
+        self.fmt = fmt
 
-update(probe_ok, display_wrong)  # type error
+    def __call__(self, func):
+        def clocked(*_args):
+            t0 = time.perf_counter()
+            _result = func(*_args)
+            elapsed = time.perf_counter() - t0
+            name = func.__name__
+            args = ', '.join(repr(arg) for arg in _args)
+            result = repr(_result)
+            print(self.fmt.format(**locals()))
+            return _result
+        return clocked
 
-def display_ok(temperature: complex) -> None:
-    print(temperature)
 
-update(probe_ok, display_ok)  # OK
+if __name__ == '__main__':
+
+    @clock2('{name}({args}) dt={elapsed:0.3f}s')
+    def snooze(seconds):
+        """Just wait for some time"""
+        time.sleep(seconds)
+
+    for i in range(3):
+        snooze(.123)
+
+    print(snooze.__name__)
+    print(snooze.__doc__)
--- Learning/130_Fluent_Python/re_search_fp.py
+++ Learning/130_Fluent_Python/re_search_fp.py
@@ -5,12 +5,25 @@
 
 import re
 
-IMPORT_RE = re.compile(r'^>>> *import +[^ ,]+')
+# IMPORT_RE = re.compile(r'^>>> *import +[^ ,]+')
+BINARY_RE = re.compile(r'[\x01-\x1f]')
 
-source = r"C:\DocumentsOD\Doc dev\Python\Fluent Python 2nd ed\Fluent Python (2nd ed^J 2022) - [O'Reilly] - Luciano Ramalho 86.txt"
+def print_special(line: str) -> None:
+    for c in line:
+        if c < ' ' and c!='\x0a':
+            print(f'«{ord(c):02X}»', end='')
+        else:
+            print(c, end='')
+    print()
+
+
+source = r"C:\DocumentsOD\Doc dev\Python\Fluent Python 2nd ed\Fluent Python (2nd ed, 2022) - [O'Reilly] - Luciano Ramalho 89.txt"
+ln = 0
 with open(source, 'rt', encoding='utf-8') as sr:
     line: str
     for line in sr.readlines():
-        if IMPORT_RE.match(line):
-            print(line.rstrip())
-
+        ln += 1
+        l = line[:-1]
+        if BINARY_RE.match(l):
+            print(f'{ln:>6} ', end='')
+            print_special(l)
