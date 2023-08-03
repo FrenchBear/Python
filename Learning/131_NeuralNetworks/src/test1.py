86537651ab18bc7992a939c83280dac0042558e9
French Bear <FrenchBear38@outlook.com>
2023-08-03 22:16:25 +0200

    save

--- Learning/131_NeuralNetworks/src/test1.py
+++ Learning/131_NeuralNetworks/src/test1.py
@@ -1,6 +1,17 @@
 import network
 import mnist_loader
+from pprint import pprint
+import numpy as np
 
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net = network.Network([2, 3, 1])
+pprint(net.num_layers)
+pprint(net.sizes)
+print('\nBiases')
+pprint(net.biases)
+print('\nWeights')
+pprint(net.weights)
+
+# training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+# net = network.Network([784, 30, 10])
+# net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
---
+++ Learning/131_NeuralNetworks/src/testnumpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+from pprint import pprint
+
+# Miscellaneous functions
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0 / (1.0 + np.exp(-z))
+
+a = np.array([2,3]).reshape(2,1) # Column vector 2 elements
+b = np.array([0.2,0.7,0.3]).reshape(3,1) # Column vector 3 elements
+w = np.array([0.1,0.2,0.3,0.4,0.5,0.6]).reshape(3,2)  # Matrix 3 rows 2 columns
+
+print('\na')
+pprint(a)
+print('\nb')
+pprint(b)
+print('\nw')
+pprint(w)
+
+a2 = np.dot(w,a)+b
+print('\na2')
+pprint(a2)
+
+a3 = sigmoid(a2)
+print('\na3')
+pprint(a3)
