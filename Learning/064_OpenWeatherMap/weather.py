32f11105e35b38bef74941d39e35aa6cefa32fb6
French Bear <FrenchBear38@outlook.com>
2024-05-02 00:28:30 +0200

    Update weather.py for OpenWeather OneCall API 3.0

--- Learning/064_OpenWeatherMap/weather.py
+++ Learning/064_OpenWeatherMap/weather.py
@@ -3,6 +3,7 @@
 # From https://www.hackster.io/gatoninja236/real-time-weather-with-raspberry-pi-4-ad621f
 #
 # 2019-08-28    PV
+# 2024-04-30    PV      Subscribed to OpenWeather OneCall API 3.0 to replace 2.5 API, free up to 1000 calls/day
 
 import time
 import requests
@@ -11,16 +12,25 @@ from pprint import pprint
 with open(r'C:\Utils\Local\openweathermap.txt', encoding='utf_8') as f:
     api_key = f.read()
 
-settings = {
-    'api_key':api_key,
-    'zip_code':'38000',
-    'country_code':'fr',
-    'temp_unit':'metric'} #unit can be metric, imperial, or kelvin
+# settings25 = {
+#     'api_key':api_key,
+#     'zip_code':'38000',
+#     'country_code':'fr',
+#     'temp_unit':'metric'} #unit can be metric, imperial, or kelvin
 
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid={0}&zip={1},{2}&units={3}"
+# Saint-ismier, 168 allée de la bâtie
+lat = '45.238272'
+lon = '5.8512146'
+
+# Discontinued end of June 2024
+# BASE_URL_25 = "http://api.openweathermap.org/data/3.0/weather?appid={0}&zip={1},{2}&units={3}"
+
+# https://openweathermap.org/api/one-call-3
+BASE_URL_30 = "http://api.openweathermap.org/data/3.0/onecall?appid={0}&lat={1}&lon={2}&units=metric&exclude=alerts,daily,minutely,hourly"
 
 while True:
-    final_url = BASE_URL.format(settings["api_key"],settings["zip_code"],settings["country_code"],settings["temp_unit"])
+    # final_url = BASE_URL_25.format(settings25["api_key"],settings25["zip_code"],settings25["country_code"],settings25["temp_unit"])
+    final_url = BASE_URL_30.format(api_key, lat, lon)
     weather_data = requests.get(final_url).json()
     pprint(weather_data)
-    time.sleep(20) #get new data every 20 seconds
+    time.sleep(20)  # get new data every 20 seconds
