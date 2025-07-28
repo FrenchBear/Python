306dff792a48d277f85d95e0e719b909e8cf7c4f
French Bear <frenchbear38@outlook.com>
2025-07-29 01:26:53 +0200

    copy_non_music_files.py

---
+++ Learning/126_MusicATrier/copy_non_music_files.py
@@ -0,0 +1,21 @@
+# copy_non_music_files.py
+# Goldwave batch processing does not copy non-music files
+#
+# 2025-07-28    PV
+
+from common_fs import get_all_files, extension_part, folder_part
+import shutil
+import os
+
+source = r'\\terazalt\music\Pierre\A_Trier\A_Trier Brut'
+destination = r"C:\Temp\MP3320"
+doit = True
+
+for filefp in get_all_files(source):
+    ext = extension_part(filefp ).casefold()
+    if not ext in ['.flac', '.mp3', '.db', '.log']:
+        destfp = filefp.replace(source, destination).replace('.jpeg', '.jpg')
+        print(filefp, "->", destfp)
+        if doit:
+            os.makedirs(folder_part(destfp), exist_ok=True)
+            shutil.copy2(filefp, destfp)
