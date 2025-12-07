aa06b5cdc05e5a1723090df8c203001302ff0a24
French Bear <frenchbear38@outlook.com>
2025-12-07 01:11:57 +0100

    Save

---
+++ Learning/168_BdfFont/test.py
@@ -0,0 +1,18 @@
+
+# pat = [ 0x70, 0x88, 0x08, 0x10, 0x20, 0x00, 0x20, ]
+
+# for p in pat:
+#     s = f"{p:08b}".replace('0', '.').replace('1', '#')
+#     print(s)
+
+i=32
+while i<128:
+    s=''
+    for j in range(8):
+        c = i+j
+        if c==ord("'") or c==ord("\\"):
+            s+='\\'
+        s+=chr(c)
+    
+    print(f'<local:Segment14Screen8 Value="{s}" Width="400" />')
+    i+=8
