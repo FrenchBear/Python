932ab01b19576aa45b2fd17b2f5125c2a5021f5d
French Bear <frenchbear38@outlook.com>
2025-12-14 00:12:59 +0100

    Save FindLengthPath (rebuilt)

---
+++ Learning/169_Find_Long_Path/dirlength.py
@@ -0,0 +1,15 @@
+# Find folders with a give, number of files
+
+import os
+
+#!/usr/bin/env python3
+"""
+Find all subfolders under a root that contain exactly N files.
+"""
+
+def find_dirs_with_exact_file_count(root: str, target_count: int):
+    for dirpath, dirnames, filenames in os.walk(root):
+        if len(filenames) == target_count:
+            print(dirpath)
+
+find_dirs_with_exact_file_count(r"D:\Pierre\HomeShared", 26)
--- Learning/169_Find_Long_Path/flp.py
+++ Learning/169_Find_Long_Path/flp.py
@@ -1,13 +1,47 @@
+# flp.py
+# Find files longer than lmax char and truncates them
+#
+# 2025-12-13    PV
+
+from collections import defaultdict
 import os
+import sys
+from common_fs import get_all_files
 
-#!/usr/bin/env python3
-"""
-Find all subfolders under a root that contain exactly N files.
-"""
+root = r'd:\Pierre\HomeShared\MusicPV'
+lmax = 210
 
-def find_dirs_with_exact_file_count(root: str, target_count: int):
-    for dirpath, dirnames, filenames in os.walk(root):
-        if len(filenames) == target_count:
-            print(dirpath)
+dd = defaultdict(list)
+for filefp in get_all_files(root):
+    path, file = os.path.split(filefp)
+    basename, ext = os.path.splitext(file)
 
-find_dirs_with_exact_file_count(r"D:\Kaforas\HomeSharedKB", 58)
+    if len(filefp) > lmax:
+        itr = lmax - len(path) - 1 - len(ext)
+        if itr <= 0:
+            breakpoint()
+        while basename[itr] != ' ':
+            itr -= 1
+        if itr <= 0:
+            breakpoint()
+        tr = os.path.join(path, basename[:itr] + ext)
+        dd[tr].append((path, basename, ext))
+
+# Check that no two truncated files would get the same name
+pb = False
+for tr in dd.keys():
+    if len(dd[tr]) > 1:
+        print("Dup trunc")
+        pb = True
+if pb:
+    sys.exit(1)
+
+print(len(dd), 'filename(s) to truncate')
+
+for nn in dd.keys():
+    path, basename, ext = dd[nn][0]
+    on = os.path.join(path, basename + ext)
+    print(on)
+    print(nn)
+    print()
+    os.rename(on, nn)
